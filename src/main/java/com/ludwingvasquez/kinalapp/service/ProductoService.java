7d48f1aabdabba4a280bf54527f4793cd2e085b0
lvasquez-2025014 <ludwingivanvasqueznavas@gmail.com>
2026-03-28 17:18:16 -0600

    feat: Se sube ProdructoService.java

--- src/main/java/com/ludwingvasquez/kinalapp/service/ProductoService.java
+++ src/main/java/com/ludwingvasquez/kinalapp/service/ProductoService.java
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-
+//
 @Service
 public class ProductoService implements IProductoService {
 
