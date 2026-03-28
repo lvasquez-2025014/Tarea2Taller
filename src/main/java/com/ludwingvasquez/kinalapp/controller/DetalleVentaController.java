f86ca64e2678b0254f5f2f2e071b4ea674154ed2
lvasquez-2025014 <ludwingivanvasqueznavas@gmail.com>
2026-03-28 17:45:08 -0600

    feat: Se sube DetalleVentaController.java

--- src/main/java/com/ludwingvasquez/kinalapp/controller/DetalleVentaController.java
+++ src/main/java/com/ludwingvasquez/kinalapp/controller/DetalleVentaController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//
 @RestController
 @RequestMapping("/detalles_ventas")
 public class DetalleVentaController {
