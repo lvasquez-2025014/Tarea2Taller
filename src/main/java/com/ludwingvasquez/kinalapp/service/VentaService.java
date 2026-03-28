85ce8b6d90a9e2f9e4160a8919113b9608444a0c
lvasquez-2025014 <ludwingivanvasqueznavas@gmail.com>
2026-03-28 17:38:05 -0600

    feat: Se sube VentaService.java

--- src/main/java/com/ludwingvasquez/kinalapp/service/VentaService.java
+++ src/main/java/com/ludwingvasquez/kinalapp/service/VentaService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-
+//
 @Service
 public class VentaService implements IVentaService {
 
