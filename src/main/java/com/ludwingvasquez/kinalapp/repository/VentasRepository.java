0f55207f70f1bf647657de234358e5db15fb1c16
lvasquez-2025014 <ludwingivanvasqueznavas@gmail.com>
2026-04-15 20:54:03 -0600

    feat: agrego mejoras a VentasRepository

--- src/main/java/com/ludwingvasquez/kinalapp/repository/VentasRepository.java
+++ src/main/java/com/ludwingvasquez/kinalapp/repository/VentasRepository.java
@@ -4,7 +4,7 @@ import com.ludwingvasquez.kinalapp.entity.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-//
+
 @Repository
 public interface VentasRepository extends JpaRepository<Venta, Long> {
 
