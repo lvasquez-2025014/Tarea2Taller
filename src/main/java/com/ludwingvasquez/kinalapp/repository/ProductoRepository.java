ff1eeb975acfe5115f3f130766f220c6ee045db1
lvasquez-2025014 <ludwingivanvasqueznavas@gmail.com>
2026-04-15 20:54:01 -0600

    feat: agrego mejoras a ProductoRepository

--- src/main/java/com/ludwingvasquez/kinalapp/repository/ProductoRepository.java
+++ src/main/java/com/ludwingvasquez/kinalapp/repository/ProductoRepository.java
@@ -2,9 +2,11 @@ package com.ludwingvasquez.kinalapp.repository;
 
 import com.ludwingvasquez.kinalapp.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-//
+
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
     List<Producto> findByEstado(Long estado);
 }
