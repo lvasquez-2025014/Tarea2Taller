d4783885f87f291c7edfb9c79d1f3f7fc87098dc
lvasquez-2025014 <ludwingivanvasqueznavas@gmail.com>
2026-04-15 20:53:59 -0600

    feat: agrego mejoras a ClienteRepository

--- src/main/java/com/ludwingvasquez/kinalapp/repository/ClienteRepository.java
+++ src/main/java/com/ludwingvasquez/kinalapp/repository/ClienteRepository.java
@@ -2,9 +2,11 @@ package com.ludwingvasquez.kinalapp.repository;
 
 import com.ludwingvasquez.kinalapp.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-//.
+
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, String> {
     List<Cliente> findByEstado(Integer estado);
 }
